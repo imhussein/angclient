1889e53bd43fddba22a76f77cc5fd1d4a2173017
mh10445 <hussein@blockgemini.com>
2019-09-26 17:44:58 +0400

    Add

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,7 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html"
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  ngOnInit() {
+    // Login Form
+    this.loginForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null)
+    });
+  }
+}
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { LoginComponent } from "src/app/components/auth/login/login.component";
 import { RegisterComponent } from "src/app/components/auth/register/register.component";
 import { AuthRotuingModule } from "./auth-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [AuthRotuingModule],
+  imports: [AuthRotuingModule, ReactiveFormsModule],
   exports: [AuthRotuingModule],
   providers: []
 })
